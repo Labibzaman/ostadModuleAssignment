1a40f838447ff10c44fdf528b86b3674999e00a5
Labibuzzaman Labib <mdlabib315@gmail.com>
2023-08-26 21:25:59 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
-import 'package:ostad_dart_assignment/savignsAcoount.dart';
+import 'package:ostadmoduleassignmentss/savignsAcoount.dart';
 
-void main(){
-  SavngsAccount saveaccount = SavngsAccount(interestRate: 10, accountNumber: 210155555, balance: 1000);
-
+void main() {
+  SavingsAccount saveaccount =
+  SavingsAccount(accountNumber: 210155555, balance: 1000, interestRate: 0.05);
+  print('Savings Account Balance: ${saveaccount.balance}');
   saveaccount.deposit(2000);
-  print('Savings Account Balance After Deposit : ${saveaccount.balance}');
+  print('Savings Account Balance After Deposit: ${saveaccount.balance}');
+
+  saveaccount.interest();
+  print('Savings Account Balance after interest: ${saveaccount.balance}');
+
   saveaccount.withdraw(1000);
-  print('Savigns Account Balance after withdrawal with interest rate :${saveaccount.balance}');
+  print('Savings Account Balance after withdrawal with interest: ${saveaccount.balance}');
 
+  print('');
 
-
-  CurrentAccount curAccount = CurrentAccount(balance: 2000, accountNumber: 412540012, overdraftLimit: 1000);
+  CurrentAccount curAccount = CurrentAccount(accountNumber: 412540012, balance: 2000, overdraftLimit: 1000);
+  print('Current Balance: ${curAccount.balance}');
   curAccount.deposit(5000);
-  print('Balance After Deposit : ${curAccount.balance}');
-curAccount.withdraw(500);
-print('Balace After WithDrwal : ${curAccount.balance}');
+  print('Current Balance After Deposit: ${curAccount.balance}');
 
-
+  curAccount.withdraw(500);
+  print('Current Balance after withdrawal: ${curAccount.balance}');
 }
-
--- lib/savignsAcoount.dart
+++ lib/savignsAcoount.dart
@@ -1,47 +1,69 @@
 abstract class Account {
   int accountNumber;
-  double balance=0;
-  Account(this.accountNumber,this.balance);
+  double balance = 0;
 
-  void deposit(double amount){
-    balance +=amount;
+  Account(this.accountNumber, this.balance);
+
+  void deposit(double amount) {
+    balance += amount;
   }
-  withdraw(double amount);
+
+  void withdraw(double amount);
 }
 
-
-class SavngsAccount extends Account{
+class SavingsAccount extends Account {
   double interestRate;
-  SavngsAccount ({required this.interestRate,required accountNumber, required double balance})
-      : super(accountNumber,balance );
+
+  SavingsAccount(
+      {required int accountNumber, required double balance, required this.interestRate})
+      : super(accountNumber, balance);
+
   @override
-  withdraw(double amount) {
- if( amount <= balance){
-   balance -= amount;
-   balance += balance * interestRate;
- }else
-   {
-     print('Insufficient Funds');
-   }
+  void deposit(double amount) {
+    balance += amount;
+    print('Succesfully deposited $amount');
   }
 
+  @override
+  void withdraw(double amount) {
+    if (amount <= balance) {
+      balance -= amount;
+      interest();
+      print('Withdrawal of $amount is successful');
+    } else {
+      print('Insufficient funds');
+    }
+  }
+
+  void interest() {
+    balance += balance * interestRate;
+  }
 }
 
 class CurrentAccount extends Account {
   double overdraftLimit = 0;
 
-  CurrentAccount({required accountNumber, required double balance,required this.overdraftLimit})
-      : super(accountNumber,balance );
+  CurrentAccount(
+      {required int accountNumber, required double balance, required this.overdraftLimit})
+      : super(accountNumber, balance);
+
+  @override
+  void deposit(double amount) {
+    balance += amount;
+    print('Succesfully deposited $amount');
+  }
 
   @override
   void withdraw(double amount) {
-    double totalLimit =balance + overdraftLimit;
-  if(amount <= totalLimit){
-    balance -=amount;
-  } else{
-    print("insuffient balance");
+    double totalLimit = balance + overdraftLimit;
+
+    if (amount <= totalLimit) {
+      balance -= amount;
+      print('Withdrawal of $amount is successful');
+    } else {
+      print("Insufficient balance");
+    }
   }
-}
 
 
 }
