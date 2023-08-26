378d5895ce13165ec796bbdb3165fdb48fb5d6e3
Labibuzzaman Labib <mdlabib315@gmail.com>
2023-08-26 21:33:16 +0600

    updated

--- lib/savignsAndCurrent.dart
+++ lib/savignsAndCurrent.dart
@@ -1,4 +1,5 @@
 abstract class Account {
+  ///initialize account number and balance
   int accountNumber;
   double balance = 0;
 
@@ -8,17 +9,17 @@ abstract class Account {
     balance += amount;
   }
 
-  void withdraw(double amount);
+  void withdraw(double amount);///abstract method
 }
 
-class SavingsAccount extends Account {
-  double interestRate;
+class SavingsAccount extends Account { ///savings account inherits Account class
+  double interestRate; ///additional property for savings Account
 
   SavingsAccount(
       {required int accountNumber, required double balance, required this.interestRate})
-      : super(accountNumber, balance);
+      : super(accountNumber, balance); ///there is a super keyword means this is inheriting from account class
 
-  @override
+  @override ///deposit method
   void deposit(double amount) {
     balance += amount;
     print('Succesfully deposited $amount');
@@ -28,24 +29,26 @@ class SavingsAccount extends Account {
   void withdraw(double amount) {
     if (amount <= balance) {
       balance -= amount;
-      interest();
+      interest();  ///calling
+
       print('Withdrawal of $amount is successful');
     } else {
       print('Insufficient funds');
     }
   }
 
-  void interest() {
+  void interest() { ///created a interest method to calculate balance with interest
     balance += balance * interestRate;
   }
 }
 
 class CurrentAccount extends Account {
+  ///overdraft limit set for current account
   double overdraftLimit = 0;
 
   CurrentAccount(
       {required int accountNumber, required double balance, required this.overdraftLimit})
-      : super(accountNumber, balance);
+      : super(accountNumber, balance); ///there is a super keyword means this is inheriting from account class
 
   @override
   void deposit(double amount) {
@@ -55,7 +58,7 @@ class CurrentAccount extends Account {
 
   @override
   void withdraw(double amount) {
-    double totalLimit = balance + overdraftLimit;
+    double totalLimit = balance + overdraftLimit; /// stored the total limit
 
     if (amount <= totalLimit) {
       balance -= amount;
